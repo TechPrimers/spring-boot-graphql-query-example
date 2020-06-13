e5d9e9790cfe0dac29c317b9e396900e17af62b8
Smit <smitshah95@hotmail.com>
2020-06-13 23:04:19 +0800

    added cache impl

--- src/main/java/com/techprimers/graphql/springbootgrapqlexample/service/datafetcher/AllBooksDataFetcher.java
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/service/datafetcher/AllBooksDataFetcher.java
@@ -2,21 +2,31 @@ package com.techprimers.graphql.springbootgrapqlexample.service.datafetcher;
 
 import com.techprimers.graphql.springbootgrapqlexample.model.Book;
 import com.techprimers.graphql.springbootgrapqlexample.repository.BookRepository;
+import com.techprimers.graphql.springbootgrapqlexample.service.CacheService;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Component
 public class AllBooksDataFetcher implements DataFetcher<List<Book>>{
 
     @Autowired
-    BookRepository bookRepository;
+    CacheService cacheService;
 
     @Override
     public List<Book> get(DataFetchingEnvironment dataFetchingEnvironment) {
-        return bookRepository.findAll();
+        try {
+            return cacheService.getAllBooks();
+        } catch (ExecutionException e) {
+            return new ArrayList<>();
+        }
     }
 }
