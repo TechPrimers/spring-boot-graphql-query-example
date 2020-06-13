938455e9b3169ed723dd9e4d111642fc512f7ae1
Smit <smitshah95@hotmail.com>
2020-06-13 23:04:19 +0800

    added cache impl

---
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/service/CacheService.java
@@ -0,0 +1,54 @@
+package com.techprimers.graphql.springbootgrapqlexample.service;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.techprimers.graphql.springbootgrapqlexample.model.Book;
+import com.techprimers.graphql.springbootgrapqlexample.repository.BookRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class CacheService {
+
+    private LoadingCache<String, List<Book>> cache;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @PostConstruct
+    private void InitCache() throws IOException {
+        this.cache = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .expireAfterAccess(24, TimeUnit.HOURS)
+                .recordStats()
+                .build(new CacheLoader<String, List<Book>>() {
+                    @Override
+                    public List<Book> load(String s) throws IOException {
+                        return getBooks();
+                    }
+                });
+    }
+
+    private List<Book> getBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getByIsn(String id) throws ExecutionException {
+        log.info(">> id {} >> cache stats {}", id, cache.stats());
+        return cache.get(id).stream().filter(book -> book.getIsn().equals(id)).findFirst().get();
+    }
+
+    public List<Book> getAllBooks() throws ExecutionException {
+        log.info(">> all >> cache stats {}", cache.stats());
+        return cache.get("");
+    }
+}
--- src/main/java/com/techprimers/graphql/springbootgrapqlexample/service/datafetcher/BookDataFetcher.java
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/service/datafetcher/BookDataFetcher.java
@@ -2,22 +2,30 @@ package com.techprimers.graphql.springbootgrapqlexample.service.datafetcher;
 
 import com.techprimers.graphql.springbootgrapqlexample.model.Book;
 import com.techprimers.graphql.springbootgrapqlexample.repository.BookRepository;
+import com.techprimers.graphql.springbootgrapqlexample.service.CacheService;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
 
 @Component
 public class BookDataFetcher implements DataFetcher<Book>{
 
     @Autowired
-    BookRepository bookRepository;
+    CacheService cacheService;
 
     @Override
     public Book get(DataFetchingEnvironment dataFetchingEnvironment) {
 
         String isn = dataFetchingEnvironment.getArgument("id");
 
-        return bookRepository.findById(isn).get();
+        try {
+            return cacheService.getByIsn(isn);
+        } catch (ExecutionException e) {
+            return null;
+        }
     }
 }
