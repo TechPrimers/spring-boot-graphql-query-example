61991e12358d63fff92a0d03844c168a2112fec6
Ram <khandelwalsriram24@gmail.com>
2022-12-30 14:30:54 +0530

    Migration to spring-boot v3.1.0 and java 17 - updated the spring boot version to 3.0.1 - updated jdk version to 17 from 11 - fixed imports for to use jakarta package istead of javax - tested the APIs, working fine
    Author: @ram2408

--- src/main/java/com/techprimers/graphql/springbootgrapqlexample/model/Book.java
+++ src/main/java/com/techprimers/graphql/springbootgrapqlexample/model/Book.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @AllArgsConstructor
 @NoArgsConstructor
